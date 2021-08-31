ebcb1eed4fe2958205abdcfb0440828391322dd2
Ashima0301 <86609222+Ashima0301@users.noreply.github.com>
2021-08-31 16:09:26 +0530

    Delete distance between two points.py

--- distance between two points.py
+++
@@ -1,6 +0,0 @@
-x1 = float(input('Enter x1: '))
-y1 = float(input('Enter y1: '))
-x2 = float(input('Enter x2: '))
-y2 = float(input('Enter y2: '))
-d = ( (x2-x1)**2 + (y2-y1)**2 )
-print('Distance = %f' %(d))
